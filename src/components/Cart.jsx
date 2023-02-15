c697bd55b6279dfdc5156324f0ffbc7085a5adc3
magnatavr <benoyevskiy@mail.ru>
2023-02-15 16:34:31 +0300

    done sum tasks

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
-export default function Cart () {
+export default function Basket() {
   return (
-    <div>Cart </div>
-  )
+    <div className="shopping-cart">
+      <div className="title">Shopping Bag</div>
+
+      <div className="item" style={{ display: "flex", flexDirection: "row" ,justifyContent:'space-around'}}>
+        {/* delete item  */}
+        <div className="buttons">
+          <button type="button" className="delete-btn">
+            delete
+          </button>
+        </div>
+        {/* card */}
+        <div className="image">
+          <img src="item-1.png" alt="" />
+        </div>
+
+        <div className="description">
+          <span>Common Projects</span>
+          <span>Bball High</span>
+          <span>White</span>
+        </div>
+        {/* price */}
+        <div className="total-price">$549</div>
+        {/* button buy */}
+      </div>
+      <button type="button">buy</button>
+    </div>
+  );
 }
