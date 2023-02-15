4600ee0bf2b56384ab1abb4912d7e0eadc26820f
Samurai <a.i.kamenev90@icloud.com>
2023-02-15 18:49:23 +0300

    onecard hardcode

--- src/components/card/AddCard.jsx
+++ src/components/card/AddCard.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
+import React from 'react';
 
 export default function AddCard() {
   return (
-    <div class="container">  
-  <form id="contact" action="" method="post">
-    <h3>Add your card</h3>
-    {/* <h4>Contact us today, and get reply with in 24 hours!</h4> */}
-    <fieldset>
-      <input placeholder="Card's name" type="text" tabindex="1" required autofocus/>
-    </fieldset>
-    <div class="input-group">
-  <input placeholder="Add price" type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)"/>
-</div>
-    <fieldset>
-      <input placeholder="Add URL img" type="url" tabindex="1" required autofocus/>
-    </fieldset>
-    <fieldset>
-    <div class="input-group mb-3">
-  <input type="text" class="form-control" aria-label="Text input with dropdown button"/>
-  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-  <ul class="dropdown-menu dropdown-menu-end">
-    <li><a class="dropdown-item" href="#">Good</a></li>
-    <li><a class="dropdown-item" href="#">Average action</a></li>
-    <li><a class="dropdown-item" href="#">Bed</a></li>
-  </ul>
-</div>
-    </fieldset>
-    <fieldset>
-      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-    </fieldset>
-  </form>
- 
-  
-</div>
-  )
+    <div className="container">
+      <form id="contact" action="" method="post">
+        <h3>Add your card</h3>
+        {/* <h4>Contact us today, and get reply with in 24 hours!</h4> */}
+        <fieldset>
+          <input placeholder="Card's name" type="text" tabIndex="1" required autoFocus />
+        </fieldset>
+        <div className="input-group">
+          <input placeholder="Add price" type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
+        </div>
+        <fieldset>
+          <input placeholder="Add URL img" type="url" tabIndex="1" required autoFocus />
+        </fieldset>
+        <fieldset>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" aria-label="Text input with dropdown button" />
+            <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+            <ul className="dropdown-menu dropdown-menu-end">
+              <li><a className="dropdown-item" href="#">Good</a></li>
+              <li><a className="dropdown-item" href="#">Average action</a></li>
+              <li><a className="dropdown-item" href="#">Bed</a></li>
+            </ul>
+          </div>
+        </fieldset>
+        <fieldset>
+          <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+        </fieldset>
+      </form>
+
+    </div>
+  );
 }
--- src/components/card/OneCard.jsx
+++ src/components/card/OneCard.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+
+const card = {
+  name: 'Mountain', img: 'https://m.media-amazon.com/images/I/41uQsn5uK0L._AC_.jpg', price: '555', condition: 'Good',
+};
 
 export default function OneCard() {
   return (
-    <div>OneCard</div>
-  )
+    <div className="card col-3 mt-1">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={card.img} className="img-fluid rounded-start" alt={card.name} />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{card.name}</h5>
+            <p className="card-text">
+              Price:
+              {' '}
+              {card.price}
+              {' '}
+              $
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Condition:
+                {' '}
+                {card.condition}
+                {' '}
+              </small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
 }
