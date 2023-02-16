e5cb8a8b40a51d44948f9daadcc8b5e06c44fb53
Andrey Vorozhtsov <pilottu154b2@gmail.com>
2023-02-16 12:31:39 +0300

    merge2

--- src/components/card/OneCard.jsx
+++ src/components/card/OneCard.jsx
@@ -3,28 +3,35 @@ import AppStore from '../../store';
 
 export default function OneCard() {
   const card = AppStore((state) => state.card);
-console.log(card);
+  console.log(card);
   return (
-
-    <div className="col-md-4">
-      <div className="card" style={{ width: '18rem' }}>
-        <img src="https://i.imgur.com/ZTkt4I5.jpg" className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn mr-2">
-            <i className="fas fa-link" />
-            {' '}
-            Visit Site
-          </a>
-          <a href="#" className="btn ">
-            <i className="fab fa-github" />
-            {' '}
-            Github
-          </a>
+    <div className="card col-3 mt-1">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={card.img} className="img-fluid rounded-start" alt={card.name} />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">{card.name}</h5>
+            <p className="card-text">
+              Price:
+              {' '}
+              {card.price}
+              {' '}
+              $
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                Condition:
+                {' '}
+                {card.condition}
+                {' '}
+              </small>
+            </p>
+          </div>
         </div>
       </div>
     </div>
+
   );
 }
