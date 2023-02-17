94995a4f0e9ad02a65f7cab2e0f8ef61eaba38b1
LeonidZolotovskiy <leonid_zolotovkiy@bk.ru>
2023-02-17 16:03:59 +0300

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+        <div>
+          <ul style={{
+            display: 'flex', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center',
+          }}
+          >
+            <li className="m-2">
+              Москва ул. Орджоникидзе, 11с10
+              <br />
+              +7 (777) 777-77-77
+            </li>
+            <li className="m-2">
+              Санкт-Петербург ул. Кирочная, 19
+              <br />
+              +7 (111) 111-11-11
+            </li>
+            <li className="m-2">
+              <div className="socials" style={{ display: 'flex', alignItems: 'center', columnGap: '20px' }}>
+                <div style={{ width: '20px', height: '20px' }}>
+                  <a href="/#"><img style={{ width: '20px', height: '20px' }} src="/img/telegram.png" alt="telegram" /></a>
+                </div>
+                <div style={{ width: '20px', height: '20px' }}>
+                  <a href="/#"><img style={{ width: '20px', height: '20px' }} src="/img/instagram.png" alt="instagram" /></a>
+                  {' '}
+                </div>
+                <div style={{ width: '20px', height: '20px' }}>
+                  <a href="/#"><img style={{ width: '20px', height: '20px' }} src="/img/vk.png" alt="instagram" /></a>
+                  {' '}
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+    </footer>
+  );
+}
